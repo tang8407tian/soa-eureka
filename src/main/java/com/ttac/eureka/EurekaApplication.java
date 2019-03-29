e6ff92b735bcf76cd83e3f88085b517c541cbcc6
bang.tang <bang.tang@biz-united.com.cn>
2019-03-29 11:36:15 +0800

    add

---
+++ src/main/java/com/ttac/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.ttac.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * eureka注册中心
+ * @author fish
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
